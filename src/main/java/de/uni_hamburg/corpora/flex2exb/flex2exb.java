cd82c8692a4dc814cc241ebaf65f5c593566a477
Niko Partanen <nikotapiopartanen@gmail.com>
2016-09-07 16:24:31 +0200

    fixed the method, more or less

--- src/main/java/de/uni_hamburg/corpora/flex2exb/flex2exb.java
+++ src/main/java/de/uni_hamburg/corpora/flex2exb/flex2exb.java
@@ -12,11 +12,13 @@ import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.Scanner;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.xml.transform.Transformer;
@@ -62,54 +64,36 @@ public class flex2exb {
 
     }
 
-//    @POST
-//    @Path("/upload")
-//    @Consumes(MediaType.MULTIPART_FORM_DATA)
-//    @Produces("text/plain")
-//    public String uploadFile(
-//            @FormDataParam("file") InputStream uploadedInputStream//,
-////            @FormDataParam("file") FormDataContentDisposition fileDetail//,
-//    //        @PathParam("lang") String language
-//    ) throws IOException, TransformerConfigurationException, TransformerException {
-//
-////		String fileName = fileDetail.getFileName();
-//        InputStream xslFile = getClass().getResourceAsStream("/xsl/interlinear-flexeaf2exmaralda.xsl");
-//
-//        if (xslFile == null) {
-//            throw new IOException("Stylesheet not found!");
-//        }
-//
-//        StreamSource xslSource = new StreamSource(xslFile);
-//
-//        // create the transformerfactory & transformer instance
-//        TransformerFactory tf = TransformerFactory.newInstance("net.sf.saxon.TransformerFactoryImpl", null);
-//        Transformer t = tf.newTransformer(xslSource);
-//        t.setParameter("language", "xas");
-//
-//        // xml comes from uploaded file
-//        StreamSource xmlSource = new StreamSource(uploadedInputStream);
-//
-//        StringWriter xmlOutWriter = new StringWriter();
-//        // do transformation
-//        t.transform(xmlSource, new StreamResult(xmlOutWriter));
-//
-//        return xmlOutWriter.toString();
-//
-////		String output = "File uploaded to : " + uploadedFileLocation;
-////		return Response.status(200).entity(output).build();
-//    }
-	@POST
-	@Path("/upload")
-	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public String uploadFile(
-			@FormDataParam("file") InputStream uploadedInputStream,
-			@FormDataParam("file") FormDataContentDisposition fileDetail) {
+    @POST
+    @Path("/upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public String uploadFile(
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail,
+            @FormDataParam("lang") String language) throws IOException, TransformerConfigurationException, TransformerException {
 
-                
-                String inputStreamString = new Scanner(uploadedInputStream,"UTF-8").useDelimiter("\\A").next();
+        InputStream xslFile = getClass().getResourceAsStream("/xsl/interlinear-flexeaf2exmaralda.xsl");
 
-		return inputStreamString;
+        if (xslFile == null) {
+            throw new IOException("Stylesheet not found!");
+        }
 
-	}
+        StreamSource xslSource = new StreamSource(xslFile);
+
+        // create the transformerfactory & transformer instance
+        TransformerFactory tf = TransformerFactory.newInstance("net.sf.saxon.TransformerFactoryImpl", null);
+        Transformer t = tf.newTransformer(xslSource);
+        t.setParameter("language", language);
+
+        // xml comes from uploaded file
+        StreamSource xmlSource = new StreamSource(uploadedInputStream);
+
+        StringWriter xmlOutWriter = new StringWriter();
+        // do transformation
+        t.transform(xmlSource, new StreamResult(xmlOutWriter));
+
+        return xmlOutWriter.toString();
+
+    }
 
 }
