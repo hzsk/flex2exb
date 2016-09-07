07da52835ccf556a16ca1e197ec81e045c94994c
Niko Partanen <nikotapiopartanen@gmail.com>
2016-09-07 15:06:44 +0200

    now the basic concept of uploading file through a form works

--- src/main/java/de/uni_hamburg/corpora/flex2exb/flex2exb.java
+++ src/main/java/de/uni_hamburg/corpora/flex2exb/flex2exb.java
@@ -5,14 +5,20 @@
  */
 package de.uni_hamburg.corpora.flex2exb;
 
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.util.Scanner;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
@@ -24,15 +30,14 @@ import javax.xml.transform.stream.StreamSource;
  *
  * @author niko
  */
-
-@Path("/")
+@Path("/file")
 public class flex2exb {
 
     @POST
-    @Path("/flex/{lang}")
-    @Produces("text/plain")  
+    @Path("/post/{lang}")
+    @Produces("text/plain")
     public String flexTransform(final InputStream input, @PathParam("lang") String language) throws TransformerConfigurationException, IOException, TransformerException {
-     
+
         InputStream xslFile = getClass().getResourceAsStream("/xsl/interlinear-flexeaf2exmaralda.xsl");
 
         if (xslFile == null) {
@@ -45,7 +50,7 @@ public class flex2exb {
         TransformerFactory tf = TransformerFactory.newInstance("net.sf.saxon.TransformerFactoryImpl", null);
         Transformer t = tf.newTransformer(xslSource);
         t.setParameter("language", language);
-        
+
         // xml comes from input
         StreamSource xmlSource = new StreamSource(input);
 
@@ -56,5 +61,55 @@ public class flex2exb {
         return xmlOutWriter.toString();
 
     }
+
+//    @POST
+//    @Path("/upload")
+//    @Consumes(MediaType.MULTIPART_FORM_DATA)
+//    @Produces("text/plain")
+//    public String uploadFile(
+//            @FormDataParam("file") InputStream uploadedInputStream//,
+////            @FormDataParam("file") FormDataContentDisposition fileDetail//,
+//    //        @PathParam("lang") String language
+//    ) throws IOException, TransformerConfigurationException, TransformerException {
+//
+////		String fileName = fileDetail.getFileName();
+//        InputStream xslFile = getClass().getResourceAsStream("/xsl/interlinear-flexeaf2exmaralda.xsl");
+//
+//        if (xslFile == null) {
+//            throw new IOException("Stylesheet not found!");
+//        }
+//
+//        StreamSource xslSource = new StreamSource(xslFile);
+//
+//        // create the transformerfactory & transformer instance
+//        TransformerFactory tf = TransformerFactory.newInstance("net.sf.saxon.TransformerFactoryImpl", null);
+//        Transformer t = tf.newTransformer(xslSource);
+//        t.setParameter("language", "xas");
+//
+//        // xml comes from uploaded file
+//        StreamSource xmlSource = new StreamSource(uploadedInputStream);
+//
+//        StringWriter xmlOutWriter = new StringWriter();
+//        // do transformation
+//        t.transform(xmlSource, new StreamResult(xmlOutWriter));
+//
+//        return xmlOutWriter.toString();
+//
+////		String output = "File uploaded to : " + uploadedFileLocation;
+////		return Response.status(200).entity(output).build();
+//    }
+	@POST
+	@Path("/upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public String uploadFile(
+			@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail) {
+
+                
+                String inputStreamString = new Scanner(uploadedInputStream,"UTF-8").useDelimiter("\\A").next();
+
+		return inputStreamString;
+
+	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,13 @@
 <html>
-<body>
-<h2>Hello World!</h2>
-</body>
+    <body>
+        <h2>FLEx to EXB conversion</h2>
+	<form action="/flex2exb/transform/file/upload" method="post" enctype="multipart/form-data">
+ 
+	   <p>
+		Select a file : <input type="file" name="file" size="4500" />
+	   </p>
+ 
+	   <input type="submit" value="Upload It" />
+	</form>
+    </body>
 </html>
