22190e928ae6225f6d0da371d065325e907d1c14
Niko Partanen <nikotapiopartanen@gmail.com>
2017-03-20 14:32:36 +0100

    this version now doesn't have unused parts
    I removed the references to the jar file as that was not fully working, and also removed unused imports

--- src/main/java/de/uni_hamburg/corpora/flex2exb/flex2exb.java
+++ src/main/java/de/uni_hamburg/corpora/flex2exb/flex2exb.java
@@ -5,31 +5,20 @@
  */
 package de.uni_hamburg.corpora.flex2exb;
 
-import com.google.common.base.Joiner;
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
@@ -38,12 +27,9 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import org.xml.sax.InputSource;
 
@@ -53,35 +39,6 @@ import org.xml.sax.InputSource;
  */
 @Path("/file")
 public class flex2exb {
-
-    @POST
-    @Path("/post/{lang}")
-    @Produces("text/plain")
-    public String flexTransform(final InputStream input, @PathParam("lang") String language) throws TransformerConfigurationException, IOException, TransformerException {
-
-        InputStream xslFile = getClass().getResourceAsStream("/xsl/interlinear-flexeaf2exmaralda.xsl");
-
-        if (xslFile == null) {
-            throw new IOException("Stylesheet not found!");
-        }
-
-        StreamSource xslSource = new StreamSource(xslFile);
-
-        // create the transformerfactory & transformer instance
-        TransformerFactory tf = TransformerFactory.newInstance("net.sf.saxon.TransformerFactoryImpl", null);
-        Transformer t = tf.newTransformer(xslSource);
-        t.setParameter("language", language);
-
-        // xml comes from input
-        StreamSource xmlSource = new StreamSource(input);
-
-        StringWriter xmlOutWriter = new StringWriter();
-        // do transformation
-        t.transform(xmlSource, new StreamResult(xmlOutWriter));
-
-        return xmlOutWriter.toString();
-
-    }
 
     @POST
     @Path("/upload")
